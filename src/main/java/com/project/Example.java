bead42d847a22e62edd034800a47de8d693a24e3
Gabriel Bonilla <gbonilla0002@mymail.lausd.net>
2025-05-30 10:16:56 -0700

    dff

---
+++ src/main/java/com/project/Example.java
@@ -0,0 +1,10 @@
+public class Example {
+    public static void main(String[] args)
+        
+        Finch bird = new Finch();
+
+        bird.setMove("F", 20, 100);
+        bird.stop();
+        bird.disconnect();
+    }
+}
